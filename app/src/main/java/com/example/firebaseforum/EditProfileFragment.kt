31103179f287b7f702114fdeda53a75c2196e53a
Mateusz Miałkas <mateusz.mialkas@gmail.com>
2023-07-08 15:14:46 +0200

    Updated Manifest(permission to access the gallery) Added function to pick image from gallery for profile img

--- app/src/main/java/com/example/firebaseforum/EditProfileFragment.kt
+++ app/src/main/java/com/example/firebaseforum/EditProfileFragment.kt
@@ -1,16 +1,24 @@
 package com.example.firebaseforum
 
+import android.app.Activity
+import android.app.Instrumentation.ActivityResult
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.navigation.fragment.findNavController
 import com.example.firebaseforum.databinding.FragmentEditProfileBinding
 
 class EditProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentEditProfileBinding
+    private lateinit var profileImage: AppCompatImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +31,30 @@ class EditProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        profileImage = binding.profileImg
+
         binding.saveButton.setOnClickListener { saveButton() }
+        binding.changeImg.setOnClickListener { pickImage() }
     }
 
     private fun saveButton() {
         findNavController().navigate(R.id.action_editProfileFragment_to_navigation_home)
     }
+
+    private fun pickImage(){
+        val pickImg = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        changeImage.launch(pickImg)
+    }
+
+    private val changeImage =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            if(it.resultCode == Activity.RESULT_OK){
+                val data = it.data
+                val imgUri = data?.data
+                profileImage.setImageURI(imgUri)
+            }
+        }
 }
